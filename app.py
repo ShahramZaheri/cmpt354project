ada15f023746682424b35d3315e9310ae7046216
Shahram Zaheri <shahram.zaheri@yahoo.com>
2021-04-01 19:02:52 -0700

    changed view of Employee tab

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, url_for, flash, redirect
 from forms import NewEmployeeForm
 import sqlite3
 import os.path
-print(os.path)
+# print(os.path)
 print(os.getcwd())
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-print("======================")
-print(BASE_DIR)
-db_path = os.path.join(BASE_DIR, "hungfung.db")
-conn = sqlite3.connect(db_path)
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# print("======================")
+# print(BASE_DIR)
+# db_path = os.path.join(BASE_DIR, "hungfung.db")
+conn = sqlite3.connect('hungfung.db')
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'cmpt354'
 #Turn the results from the database into a dictionary
@@ -25,11 +25,16 @@ def index():
 @app.route('/employee', methods=['GET', 'POST'])
 def employee():
     form=NewEmployeeForm()
-    return render_template('employee.html', form=form)
+    return render_template('employee.html')
+
+@app.route('/employee/add_new_employee', methods=['GET', 'POST'])
+def add_new_employee():
+    form=NewEmployeeForm()
+    return render_template('add_new_employee.html',form=form)
 
 @app.route('/report')
 def report():
-    conn = sqlite3.connect(db_path)
+    conn = sqlite3.connect('hungfung.db')
     conn.row_factory = dict_factory
     c = conn.cursor()
     c.execute("SELECT * FROM employee")
@@ -37,7 +42,7 @@ def report():
     return render_template('report.html', employees=employees)
    
 
-@app.route('/removeEmployee', methods=['GET', 'POST'])
+@app.route('/employee/remove_employee', methods=['GET', 'POST'])
 def removeEmployee():
     form=NewEmployeeForm()
     return render_template('removeEmployee.html', form=form)
--- forms.py
+++ forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 #Form for inserting new employee
 class NewEmployeeForm(FlaskForm):
-    employee_first_name = StringField('First Name', validators=[DataRequired(), Length(min=1, max=10)]) 
+    employee_first_name = StringField('First Name', validators=[DataRequired(), Length(min=1, max=25)]) 
     employee_middle_name = StringField('Middle Name', validators=[DataRequired(), Length(min=1, max=25)]) 
     employee_last_name = StringField('Last Name', validators=[DataRequired(), Length(min=1, max=25)])
     employee_email = StringField('Email',validators=[DataRequired(), Email()])
@@ -15,6 +15,11 @@ class NewEmployeeForm(FlaskForm):
     employee_Address= StringField('Home Address', validators=[DataRequired(), Length(min=1, max=25)])
     employee_date_of_hire = StringField('Date of Hire', validators=[DataRequired(), Length(min=1, max=25)])
     employee_date_of_birth = StringField('Date of Birth', validators=[DataRequired(), Length(min=1, max=25)])
+    
+    first_emergency_contact_full_name = StringField('Emergency Contact Full Name', validators=[DataRequired(), Length(min=1, max=25)])
+    first_emergency_contact_relationship = StringField('Relationship with Employee', validators=[DataRequired(), Length(min=1, max=25)]) 
+    first_emergency_contact_phone_number = StringField('Emergency Contact Phone Number', validators=[DataRequired(), Length(min=1, max=25)])
+
     submit = SubmitField('Add New Employee')
 
 # class removeEmployee(FlaskForm):
