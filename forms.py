f596feafbae292bb54209aa929e1b8be302bd5e9
Shahram Zaheri <shahram.zaheri@yahoo.com>
2021-04-07 00:24:34 -0700

    added a few fields to the add new employee form

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextField, TextAreaField, SelectField
+from wtforms import StringField, IntegerField, PasswordField, SubmitField, BooleanField, TextField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
@@ -15,6 +15,9 @@ class NewEmployeeForm(FlaskForm):
     employee_Address= StringField('Home Address', validators=[DataRequired(), Length(min=1, max=25)])
     employee_date_of_hire = StringField('Date of Hire', validators=[DataRequired(), Length(min=1, max=25)])
     employee_date_of_birth = StringField('Date of Birth', validators=[DataRequired(), Length(min=1, max=25)])
+    roles=["office", "operation"]
+    employee_role = SelectField('Employee works at', choices = roles, validators = [DataRequired()])
+    employee_salary = IntegerField('Employee Salary/wage', validators = [DataRequired()])
     
     first_emergency_contact_full_name = StringField('Emergency Contact Full Name', validators=[DataRequired(), Length(min=1, max=25)])
     first_emergency_contact_relationship = StringField('Relationship with Employee', validators=[DataRequired(), Length(min=1, max=25)]) 
